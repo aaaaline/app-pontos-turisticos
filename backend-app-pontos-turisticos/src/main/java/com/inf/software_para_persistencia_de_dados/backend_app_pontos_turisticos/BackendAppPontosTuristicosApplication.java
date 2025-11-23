ee866288680151fd3ad6fcc3093249996e46eb4e
Aline <alinelmcoelho@gmail.com>
2025-11-23 20:18:30 -0300

    Creating backend project

---
+++ backend-app-pontos-turisticos/src/main/java/com/inf/software_para_persistencia_de_dados/backend_app_pontos_turisticos/BackendAppPontosTuristicosApplication.java
@@ -0,0 +1,13 @@
+package com.inf.software_para_persistencia_de_dados.backend_app_pontos_turisticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAppPontosTuristicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAppPontosTuristicosApplication.class, args);
+    }
+
+}
