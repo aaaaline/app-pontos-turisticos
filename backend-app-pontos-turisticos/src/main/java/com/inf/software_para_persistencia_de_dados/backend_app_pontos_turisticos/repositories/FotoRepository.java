7eb42587f6748841596d16cc798c9ee75e8c375d
Aline <alinelmcoelho@gmail.com>
2025-12-09 21:13:59 -0300

    feat: adiciona limite de 10 fotos por ponto turístico

--- backend-app-pontos-turisticos/src/main/java/com/inf/software_para_persistencia_de_dados/backend_app_pontos_turisticos/repositories/FotoRepository.java
+++ backend-app-pontos-turisticos/src/main/java/com/inf/software_para_persistencia_de_dados/backend_app_pontos_turisticos/repositories/FotoRepository.java
@@ -1,9 +1,11 @@
 package com.inf.software_para_persistencia_de_dados.backend_app_pontos_turisticos.repositories;
 
 import com.inf.software_para_persistencia_de_dados.backend_app_pontos_turisticos.entities.Foto;
+import com.inf.software_para_persistencia_de_dados.backend_app_pontos_turisticos.entities.PontoTuristico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FotoRepository extends JpaRepository<Foto, Long> {
+    long countByPontoTuristico(PontoTuristico pontoTuristico);
 }
